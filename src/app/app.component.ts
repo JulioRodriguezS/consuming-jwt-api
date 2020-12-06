47d40ba34eccc148acdd6406cd3defc8f507697f
JulioRodriguezS <julio.rodriguez.soberano@gmail.com>
2020-12-06 18:18:33 -0500

    uploading consuming api jwt for loggin practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'consuming-jwt-api';
+  youClicked(Name: HTMLInputElement): void {
+    const name: string = Name.value;
+    alert(name);
+  }
 }
